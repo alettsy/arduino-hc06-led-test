ace2782e0a045ac2442292f2a5a2b3de2c9283ab
Alex <mimisico100@users.noreply.github.com>
2018-11-20 13:30:04 +0000

    added comment

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,29 +1,34 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial mySerial(11, 12); // use D11 as RX and D12 as TX
-int ledPin = 5; // use D5 as led pin
+int ledPin = 5;                  // use D5 as led pin
 char data;
 
-void setup(){
+void setup()
+{
   pinMode(ledPin, OUTPUT);
   mySerial.begin(9600);
   mySerial.println("Program Started!");
 }
 
-void loop(){
-  if (mySerial.available() > 0){
+void loop()
+{
+  if (mySerial.available() > 0)
+  {
     data = mySerial.read();
   }
 
-  if (data == '1'){
+  if (data == '1')
+  {
     digitalWrite(ledPin, HIGH);
     mySerial.println("ON");
   }
 
-  else if (data == '2'){
+  else if (data == '2')
+  {
     digitalWrite(ledPin, LOW);
     mySerial.println("OFF");
   }
 
-  data = '0';
+  data = '0'; // reset data to 0
 }
